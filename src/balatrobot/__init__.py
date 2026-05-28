639fe3bdfc6b28c544d0f45a70b70501f0f6b99f
S1M0N38 <bertolottosimone@gmail.com>
2026-05-28 19:33:08 +0200

    feat: export BalatroPool, InstanceInfo, and StateFile from public API
    Update __init__.py to re-export the new modules so users can
    import directly from the balatrobot package.

--- src/balatrobot/__init__.py
+++ src/balatrobot/__init__.py
@@ -2,7 +2,18 @@
 
 from balatrobot.cli.client import APIError, BalatroClient
 from balatrobot.config import Config
-from balatrobot.manager import BalatroInstance
+from balatrobot.instance import BalatroInstance
+from balatrobot.pool import BalatroPool, InstanceInfo
+from balatrobot.state import StateFile
 
 __version__ = "1.5.0"
-__all__ = ["APIError", "BalatroClient", "BalatroInstance", "Config", "__version__"]
+__all__ = [
+    "APIError",
+    "BalatroClient",
+    "BalatroInstance",
+    "BalatroPool",
+    "Config",
+    "InstanceInfo",
+    "StateFile",
+    "__version__",
+]
