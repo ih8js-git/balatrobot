5b9875e71d675bdc088470e1bba5c52c17b8a0eb
S1M0N38 <bertolottosimone@gmail.com>
2026-05-29 14:46:35 +0200

    refactor: trim __all__ to public API surface
    Remove BalatroPool, InstanceInfo, and StateFile from __all__.
    These are CLI internals, not part of the public bot-author API.
    Keep APIError, BalatroClient, BalatroInstance, Config, __version__.

--- src/balatrobot/__init__.py
+++ src/balatrobot/__init__.py
@@ -3,17 +3,12 @@
 from balatrobot.cli.client import APIError, BalatroClient
 from balatrobot.config import Config
 from balatrobot.instance import BalatroInstance
-from balatrobot.pool import BalatroPool, InstanceInfo
-from balatrobot.state import StateFile
 
 __version__ = "1.5.0"
 __all__ = [
     "APIError",
     "BalatroClient",
     "BalatroInstance",
-    "BalatroPool",
     "Config",
-    "InstanceInfo",
-    "StateFile",
     "__version__",
 ]
